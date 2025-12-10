122302db016057eae782ab7c3a86281d5d07ccb4
Pauls Toma <tomapt@udmercy.edu>
2025-12-10 09:24:09 -0500

    12/10/2025

--- projects/calculator/src/main.rs
+++ projects/calculator/src/main.rs
@@ -1,31 +1,25 @@
-fn main() {
-    
-let mut account = BankAccount{
-    balance: 5.0,
+struct BankAccount {
+    balance: u32,
+}
+
+impl BankAccount{
+    fn deposit(&mut self, amount: u32){
+        self.balance += amount;
+    }
+}
+
+impl BankAccount{
+    fn withdraw(&mut self, amount: u32){
+        self.balance -= amount;
+    }
+}
+
+fn main(){
+    let mut account = BankAccount{
+        balance: 0,
     };
-
-       println!("Balance before deposit: {}", account.balance);
-       account.deposit(5.0);
-       account.withdraw(9.0);
+    account.deposit(100);
+    account.withdraw(10);
+    println!("{}", account.balance);
 
 }
-struct BankAccount{
-       balance: f64,
-}
-
-impl BankAccount {
-    fn deposit(&mut self, amount: f64){
-        let new_balance: f64 = amount + self.balance;
-        self.balance = new_balance;
-        println!("after deposit balance: {}", new_balance);
-    }
-}
-
-impl BankAccount {
-    fn withdraw (&mut self, amount: f64){
-        let new_balance: f64 = self.balance - amount;
-        self.balance = new_balance;
-        println!("after withdraw: {}", new_balance);
-    }
-}
-
